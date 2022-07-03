f654132b5b2d66b9bcc32d62ae9bfeb33dacf9e1
SrJuan <jsebg.ruedat@gmail.com>
2022-07-03 18:55:12 -0500

    Revert "7"
    This reverts commit fe4e52705aebfcc1661231bab6acca3f3d4af63b.

--- src/app/components/empresas/empresas.component.ts
+++ src/app/components/empresas/empresas.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EmpresasService } from 'src/app/services/empresas.service'
-import Swal from 'sweetalert2'
+
 
 @Component({
     selector: 'app-empresas',
@@ -10,128 +9,33 @@ import Swal from 'sweetalert2'
     styleUrls: ['./empresas.component.css']
 })
 export class EmpresasComponent implements OnInit {
-    usuario1: any
-    nombre1: any
-    VerComentarios: any = []
-    comentarios: any
-    empresa1: any
-    tareasProyectos: any = []
-    tareas: any
     EmpresaUsuario: any
-    TareasGlobales: any
-    formularioComentario: FormGroup
-    constructor(private _Service: EmpresasService, private router: Router, private idRoute: ActivatedRoute, fb: FormBuilder) {
-        this.formularioComentario = fb.group({
-            comentarios: ['', Validators.required]
-        })
-    }
-
+    constructor(private _Service: EmpresasService,private router: Router,private idRoute: ActivatedRoute) { }
     ngOnInit(): void {
-        localStorage.setItem('proyecto1', '')
-        localStorage.setItem('proyecto2', '')
-        let usuario :any = localStorage.getItem('usuario')
-        this.usuario1 = JSON.parse(usuario)
-        if (localStorage.getItem('control-tareas') ){
-            let arregloGlobal12: any = localStorage.getItem('control-tareas')
-            let arregloGlobal123: any = JSON.parse(arregloGlobal12)
-            this.TareasGlobales = arregloGlobal123
-        } else {
-            localStorage.setItem('control-tareas', '[]')
-            let arregloGlobal12: any = localStorage.getItem('control-tareas')
-            let arregloGlobal123: any = JSON.parse(arregloGlobal12)
-            this.TareasGlobales = arregloGlobal123
-        }
         this.obtenerEmpresa()
     }
-    generadoraID() {
-
-        let random = Math.random().toString(36).substring(2);
-        let fecha = Date.now().toString(36)
-        return random + fecha;
-    }
-    agregar() {
-        console.log(this.comentarios)
-        let nombre :any = localStorage.getItem('Nombre')
-        this.nombre1 = JSON.parse(nombre)
-        let usuario :any = localStorage.getItem('usuario')
-        this.usuario1 = JSON.parse(usuario)
-        let citas = {
-            comentarios: this.formularioComentario.get('comentarios')?.value,
-            nombre: this.nombre1[0],
-            id: this.generadoraID(),
-            idEmpresa: this.comentarios[0].id,
-            usuario: this.usuario1
-        }
-        let arregloGlobal: any = localStorage.getItem('control-tareas')
-        let arregloGlobal1: any = JSON.parse(arregloGlobal)
-        if (arregloGlobal == null) {
-            this.TareasGlobales = []
-        } else {
-            this.TareasGlobales = arregloGlobal1
-        }
-        this.TareasGlobales.push(citas)
-        localStorage.setItem('control-tareas', JSON.stringify(this.TareasGlobales))
-        let arregloGlobal12: any = localStorage.getItem('control-tareas')
-        let arregloGlobal123: any = JSON.parse(arregloGlobal12)
-        this.TareasGlobales = arregloGlobal123
-        Swal.fire({
-            position: 'top-end',
-            icon: 'success',
-            title: 'Se ha comentado correctamente',
-            showConfirmButton: false,
-            timer: 1500
-        })
-        this.formularioComentario.reset()
-    }
-
-
     obtenerEmpresa() {
         let empresa: any = localStorage.getItem('Empresa')
         empresa = JSON.parse(empresa)
-        this.empresa1 = empresa[0]
+        console.log(empresa)
         this._Service.getEmpresas().subscribe(data => {
             const arregloEmpresas = data.filter((element: any) => element.name == empresa[0])
             this.EmpresaUsuario = arregloEmpresas
-            this.tareas = this.EmpresaUsuario[0].history
-            console.log(this.tareas)
+            console.log(this.EmpresaUsuario)
             localStorage.setItem('idEmpresa', JSON.stringify(this.EmpresaUsuario[0]._id))
         }, error => {
             console.log(error);
         })
     }
     proyecto1(tarea: any) {
-        localStorage.setItem('proyecto1', JSON.stringify(tarea))
-        localStorage.removeItem('proyecto2')
-        this.router.navigate(['/citas'])
+    localStorage.setItem('proyecto1', JSON.stringify(tarea))
+    localStorage.removeItem('proyecto2')
+    this.router.navigate(['/citas'])
     }
     proyecto2(tarea: any) {
-        localStorage.setItem('proyecto2', JSON.stringify(tarea))
-        localStorage.removeItem('proyecto1')
-        this.router.navigate(['/citas'])
-    }
-    seguimiento(proyecto: any) {
-        console.log(proyecto)
-        console.log(this.EmpresaUsuario[0].history)
-        const arreglo = this.EmpresaUsuario[0].history.filter((element: any) => element.proyecto === proyecto)
-        this.tareasProyectos = arreglo[0].tareas
-        console.log(this.tareasProyectos)
-    }
-    Trayendoobjeto(obj: any) {
-        this.comentarios = [obj]
-        console.log(this.comentarios)
-    }
-    ver(obj: any) {
-        const nuevoArreglo = this.TareasGlobales.filter((element: any) => element.idEmpresa == obj.id)
-        this.VerComentarios = nuevoArreglo
-    }
-    eliminar(obj: any){
-        const nuevoArreglo = this.TareasGlobales.filter((element: any) => element.id !== obj.id)
-        localStorage.setItem('control-tareas', JSON.stringify(nuevoArreglo))
-        let arregloGlobal12: any = localStorage.getItem('control-tareas')
-        let arregloGlobal123: any = JSON.parse(arregloGlobal12)
-        this.TareasGlobales = arregloGlobal123
-        const nuevoArreglo2 = this.TareasGlobales.filter((element: any) => element.idEmpresa == obj.id)
-        this.VerComentarios = nuevoArreglo
+    localStorage.setItem('proyecto2', JSON.stringify(tarea))
+    localStorage.removeItem('proyecto1')
+    this.router.navigate(['/citas'])
     }
 
 }
--- src/app/components/navbarfija/navbarfija.component.ts
+++ src/app/components/navbarfija/navbarfija.component.ts
@@ -72,7 +72,7 @@ export class NavbarfijaComponent implements OnInit {
         localStorage.setItem('usuario', '[]')
         this.popo = localStorage.getItem('Nombre')
         this.pepe = JSON.parse(this.popo)
-        this.router.navigate(['/'])
+        this.router.navigate(['/registro'])
         setTimeout(() =>{
             window.location.reload()
         },200)
